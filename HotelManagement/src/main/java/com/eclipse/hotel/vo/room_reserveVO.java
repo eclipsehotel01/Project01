cbe38dce4c840a0cbc32061b00eaa9a3910a63dc
it <it@DESKTOP-KKAQ0RK>
2017-03-21 15:02:47 +0900

    33

---
+++ HotelManagement/src/main/java/com/eclipse/hotel/vo/room_reserveVO.java
@@ -0,0 +1,65 @@
+package com.eclipse.hotel.vo;
+
+public class room_reserveVO {
+	int reservecode;
+	int m_num;
+	String check_in;
+	String check_out;
+	int person;
+	int rnum;
+	String reservedate;
+	String extracode;
+	
+	 
+	public int getReservecode() {
+		return reservecode;
+	}
+	public void setReservecode(int reservecode) {
+		this.reservecode = reservecode;
+	}
+	public int getM_num() {
+		return m_num;
+	}
+	public void setM_num(int m_num) {
+		this.m_num = m_num;
+	}
+	public String getCheck_in() {
+		return check_in;
+	}
+	public void setCheck_in(String check_in) {
+		this.check_in = check_in;
+	}
+	public String getCheck_out() {
+		return check_out;
+	}
+	public void setCheck_out(String check_out) {
+		this.check_out = check_out;
+	}
+	public int getPerson() {
+		return person;
+	}
+	public void setPerson(int person) {
+		this.person = person;
+	}
+	public int getRnum() {
+		return rnum;
+	}
+	public void setRnum(int rnum) {
+		this.rnum = rnum;
+	}
+	public String getReservedate() {
+		return reservedate;
+	}
+	public void setReservedate(String reservedate) {
+		this.reservedate = reservedate;
+	}
+	public String getExtracode() {
+		return extracode;
+	}
+	public void setExtracode(String extracode) {
+		this.extracode = extracode;
+	}
+	
+	
+	
+}
